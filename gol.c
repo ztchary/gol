a4f539e7fba06f144c2732dcaa789cdc793cb732
zacdogwater <zacsengines@gmail.com>
2026-03-23 12:21:03 -0600

    uint64_t(uff)

--- gol.c
+++ gol.c
@@ -5,13 +5,13 @@
 struct chunk {
 	int x;
 	int y;
-	unsigned long cells;
+	uint64_t cells;
 	bool occ;
 };
 
 struct chunk_map {
-	unsigned long cap;
-	unsigned long num;
+	uint64_t cap;
+	uint64_t num;
 	struct chunk *data;
 };
 
@@ -19,6 +19,7 @@ struct runner_data {
 	struct chunk_map *cur;
 	struct chunk_map *next;
 	bool *play;
+	bool *quit;
 };
 
 struct cam {
@@ -27,15 +28,15 @@ struct cam {
 	float zoom;
 };
 
-uint64_t map_hash(int x, int y) {
-	unsigned long s = ((unsigned long)x << 32) | (unsigned int)y;
+uint64_t map_hash(uint32_t x, uint32_t y) {
+	uint64_t s = ((uint64_t)x << 32) | (uint32_t)y;
 	s += 0x9e3779b97f4a7c15ULL;
 	s = (s ^ (s >> 30)) * 0xbf58476d1ce4e5b9ULL;
 	s = (s ^ (s >> 27)) * 0x41f30206230f3549ULL;
 	return s ^ (s >> 31);
 }
 
-void chunk_map_set(struct chunk_map *map, int x, int y, unsigned long cells) {
+void chunk_map_set(struct chunk_map *map, int x, int y, uint64_t cells) {
 	if (map->num == map->cap / 2) {
 		struct chunk_map new = {
 			.cap = map->cap * 2,
@@ -43,7 +44,7 @@ void chunk_map_set(struct chunk_map *map, int x, int y, unsigned long cells) {
 		};
 		new.data = malloc(sizeof(struct chunk) * new.cap);
 		memset(new.data, 0, sizeof(struct chunk) * new.cap);
-		for (unsigned long i = 0; i < map->cap; i++) {
+		for (uint64_t i = 0; i < map->cap; i++) {
 			if (!map->data[i].occ) continue;
 			struct chunk c = map->data[i];
 			chunk_map_set(&new, c.x, c.y, c.cells);
@@ -51,8 +52,8 @@ void chunk_map_set(struct chunk_map *map, int x, int y, unsigned long cells) {
 		free(map->data);
 		*map = new;
 	}
-	unsigned long off = map_hash(x, y) % map->cap;
-	for (unsigned long i = off;; i = (i + 1) % map->cap) {
+	uint64_t off = map_hash(x, y) % map->cap;
+	for (uint64_t i = off;; i = (i + 1) % map->cap) {
 		if (map->data[i].occ && map->data[i].x == x && map->data[i].y == y) {
 			map->data[i].cells = cells;
 			break;
@@ -70,17 +71,17 @@ void chunk_map_set(struct chunk_map *map, int x, int y, unsigned long cells) {
 }
 
 bool chunk_map_exists(struct chunk_map *map, int x, int y) {
-	unsigned long off = map_hash(x, y) % map->cap;
-	for (unsigned long i = off; map->data[i].occ; i = (i + 1) % map->cap) {
+	uint64_t off = map_hash(x, y) % map->cap;
+	for (uint64_t i = off; map->data[i].occ; i = (i + 1) % map->cap) {
 		if ((map->data[i].x != x || map->data[i].y != y)) continue;
 		return 1;
 	}
 	return 0;
 }
 
-unsigned long chunk_map_get(struct chunk_map *map, int x, int y) {
-	unsigned long off = map_hash(x, y) % map->cap;
-	for (unsigned long i = off; map->data[i].occ; i = (i + 1) % map->cap) {
+uint64_t chunk_map_get(struct chunk_map *map, int x, int y) {
+	uint64_t off = map_hash(x, y) % map->cap;
+	for (uint64_t i = off; map->data[i].occ; i = (i + 1) % map->cap) {
 		if ((map->data[i].x != x || map->data[i].y != y)) continue;
 		return map->data[i].cells;
 	}
@@ -103,7 +104,7 @@ void render_chunks(SDL_Renderer *renderer, struct chunk_map *map, bool show_chun
 	SDL_FRect rect;
 	int width, height;
 	SDL_GetRendererOutputSize(renderer, &width, &height);
-	for (unsigned long i = 0; i < map->cap; i++) {
+	for (uint64_t i = 0; i < map->cap; i++) {
 		if (!map->data[i].occ) continue;
 		struct chunk chunk = map->data[i];
 		rect = (SDL_FRect){
@@ -144,35 +145,35 @@ void update_chunk(struct chunk_map *cur, struct chunk_map *next, int x, int y, b
 	if (chunk_map_exists(cur, x, y) && prop) return;
 	if (chunk_map_exists(next, x, y)) return;
 
-	unsigned long cells = chunk_map_get(cur, x, y);
-	unsigned long a = chunk_map_get(cur, x - 1, y - 1);
-	unsigned long b = chunk_map_get(cur, x, y - 1);
-	unsigned long c = chunk_map_get(cur, x + 1, y - 1);
-	unsigned long d = chunk_map_get(cur, x - 1, y);
-	unsigned long e = chunk_map_get(cur, x + 1, y);
-	unsigned long f = chunk_map_get(cur, x - 1, y + 1);
-	unsigned long g = chunk_map_get(cur, x, y + 1);
-	unsigned long h = chunk_map_get(cur, x + 1, y + 1);
+	uint64_t cells = chunk_map_get(cur, x, y);
+	uint64_t a = chunk_map_get(cur, x - 1, y - 1);
+	uint64_t b = chunk_map_get(cur, x, y - 1);
+	uint64_t c = chunk_map_get(cur, x + 1, y - 1);
+	uint64_t d = chunk_map_get(cur, x - 1, y);
+	uint64_t e = chunk_map_get(cur, x + 1, y);
+	uint64_t f = chunk_map_get(cur, x - 1, y + 1);
+	uint64_t g = chunk_map_get(cur, x, y + 1);
+	uint64_t h = chunk_map_get(cur, x + 1, y + 1);
 
-	unsigned long nt = (cells >> 8) | (b << 56);
-	unsigned long nb = (cells << 8) | (g >> 56);
-	unsigned long nl = ((cells >> 1) & LBMASK) | ((d << 7) & LSMASK);
-	unsigned long nr = ((cells << 1) & RBMASK) | ((e >> 7) & RSMASK);
+	uint64_t nt = (cells >> 8) | (b << 56);
+	uint64_t nb = (cells << 8) | (g >> 56);
+	uint64_t nl = ((cells >> 1) & LBMASK) | ((d << 7) & LSMASK);
+	uint64_t nr = ((cells << 1) & RBMASK) | ((e >> 7) & RSMASK);
 
-	unsigned long dt = (d >> 8) | (a << 56);
-	unsigned long et = (e >> 8) | (c << 56);
+	uint64_t dt = (d >> 8) | (a << 56);
+	uint64_t et = (e >> 8) | (c << 56);
 	
-	unsigned long db = (d << 8) | (f >> 56);
-	unsigned long eb = (e << 8) | (h >> 56);
+	uint64_t db = (d << 8) | (f >> 56);
+	uint64_t eb = (e << 8) | (h >> 56);
 
-	unsigned long ntl = ((nt >> 1) & LBMASK) | ((dt << 7) & LSMASK);
-	unsigned long ntr = ((nt << 1) & RBMASK) | ((et >> 7) & RSMASK);
-	unsigned long nbl = ((nb >> 1) & LBMASK) | ((db << 7) & LSMASK);
-	unsigned long nbr = ((nb << 1) & RBMASK) | ((eb >> 7) & RSMASK);
+	uint64_t ntl = ((nt >> 1) & LBMASK) | ((dt << 7) & LSMASK);
+	uint64_t ntr = ((nt << 1) & RBMASK) | ((et >> 7) & RSMASK);
+	uint64_t nbl = ((nb >> 1) & LBMASK) | ((db << 7) & LSMASK);
+	uint64_t nbr = ((nb << 1) & RBMASK) | ((eb >> 7) & RSMASK);
 
-	unsigned long one = 0;
-	unsigned long two = 0;
-	unsigned long dead = 0;
+	uint64_t one = 0;
+	uint64_t two = 0;
+	uint64_t dead = 0;
 
 	#define MAGIC(q) \
 		dead |= q & one & two; \
@@ -188,7 +189,7 @@ void update_chunk(struct chunk_map *cur, struct chunk_map *next, int x, int y, b
 	MAGIC(nbl);
 	MAGIC(nbr);
 
-	unsigned long n = (one | cells) & two & ~dead;
+	uint64_t n = (one | cells) & two & ~dead;
 
 	if (n) chunk_map_set(next, x, y, n);
 
@@ -206,7 +207,7 @@ void update_chunk(struct chunk_map *cur, struct chunk_map *next, int x, int y, b
 
 void update_chunks(struct chunk_map *cur, struct chunk_map *next) {
 	chunk_map_clear(next);
-	for (unsigned long i = 0; i < cur->cap; i++) {
+	for (uint64_t i = 0; i < cur->cap; i++) {
 		if (!cur->data[i].occ) continue;
 		struct chunk c = cur->data[i];
 		update_chunk(cur, next, c.x, c.y, 0);
@@ -227,7 +228,7 @@ bool get_cell_cam(SDL_Renderer *renderer, struct chunk_map *map, struct cam cam,
 	int cellx = gx - chunkx * 8;
 	int celly = gy - chunky * 8;
 
-	unsigned long cells = chunk_map_get(map, chunkx, chunky);
+	uint64_t cells = chunk_map_get(map, chunkx, chunky);
 	return cells >> ((7 - celly) * 8 + (7 - cellx)) & 1UL;
 }
 
@@ -242,8 +243,8 @@ void set_cell_cam(SDL_Renderer *renderer, struct chunk_map *map, struct cam cam,
 	int cellx = gx - chunkx * 8;
 	int celly = gy - chunky * 8;
 
-	unsigned long cells = chunk_map_get(map, chunkx, chunky);
-	unsigned long ch = 1UL << ((7 - celly) * 8 + (7 - cellx));
+	uint64_t cells = chunk_map_get(map, chunkx, chunky);
+	uint64_t ch = 1UL << ((7 - celly) * 8 + (7 - cellx));
 	cells = (cells & ~ch) | (ch * v);
 
 	chunk_map_set(map, chunkx, chunky, cells);
@@ -253,6 +254,7 @@ void *runner(void *data) {
 	struct runner_data d = *(struct runner_data *)data;
 
 	for (;;) {
+		if (*d.quit) return NULL;
 		if (!*d.play) {
 			SDL_Delay(10);
 			continue;
@@ -276,11 +278,11 @@ int main() {
 	chunk_map_init(&cur);
 	chunk_map_init(&next);
 
-	for (int i = 0; i < 0x100; i++) {
-		unsigned long s = rand();
+	for (int i = 0; i < 0x1000; i++) {
+		uint64_t s = rand();
 		s <<= 32;
 		s |= rand();
-		chunk_map_set(&cur, (i&0xf) - 8, (i>>4) - 8, s);
+		chunk_map_set(&cur, (i&0x3f) - 8, (i>>6) - 8, s);
 	}
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -303,9 +305,10 @@ int main() {
 	SDL_Event e;
 
 	struct runner_data d = {
-		.play = &play,
 		.cur = &cur,
-		.next = &next
+		.next = &next,
+		.play = &play,
+		.quit = &quit
 	};
 
 	pthread_t tid;
